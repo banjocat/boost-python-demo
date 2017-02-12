2b70088b3a3783f7b5adb2d5c2af2b1d5060729a
banjocat <jackmuratore@gmail.com>
2017-02-12 01:02:35 -0500

    Add fibonacci

--- spam.cpp
+++ spam.cpp
@@ -3,10 +3,15 @@
 #include <vector>
 #include <iostream>
 #include <boost/python.hpp>
+#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 
+using namespace boost::python;
 using std::string;
 using std::cout;
 using std::endl;
+
+typedef std::vector<int> int_vector;
+
 
 void greet(string name)
 {
@@ -24,13 +29,33 @@ int string_length(string word)
     return word.size();
 }
 
-
+int_vector fibonacci(int num)
+{
+    int_vector result;
+    int first = 0;
+    int second = 1;
+    int next;
+    for (int i = 0; i < num; i++) {
+        if (i <= 1) {
+            next = i;
+        } else {
+            next = first + second;
+            first = second;
+            second = next;
+        }
+        result.push_back(next);
+    }
+    return result;
+}
 
 
 BOOST_PYTHON_MODULE(spam)
 {
-    using namespace boost::python;
+    class_<int_vector>("int_vector")
+        .def(vector_indexing_suite<int_vector>() );
+
     def("greet", greet);
     def("system", sys);
     def("strlen", string_length);
+    def("fibonacci", fibonacci);
 }
--- test.py
+++ test.py
@@ -5,3 +5,5 @@ spam.greet('cassie')
 spam.system('ls')
 
 print spam.strlen("1234")
+
+print [x for x in spam.fibonacci(10)]
