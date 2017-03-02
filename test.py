37390fb0d95dd90779e93793c6e0d9afa38dbf1b
banjocat <jackmuratore@gmail.com>
2017-03-02 00:41:36 -0500

    Time test c++ module vs raw python

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
 import spam
+import time
+
+def timeme(method):
+    def wrapper(*args, **kw):
+        startTime = int(round(time.time() * 1000))
+        result = method(*args, **kw)
+        endTime = int(round(time.time() * 1000))
+
+        print(endTime - startTime,'ms')
+        return result
+
+    return wrapper
 
 spam.greet('cassie')
 
@@ -6,4 +18,30 @@ spam.system('ls')
 
 print spam.strlen("1234")
 
-print [x for x in spam.fibonacci(10)]
+def python_fb(num):
+    result = []
+    first = 0
+    second = 1
+    for i in xrange(num):
+        if i <= 1:
+            next = i
+        else:
+            next = first + second
+            first = second
+            second = next
+        result.append(next)
+    return result
+
+@timeme
+def fib():
+    [x for x in spam.fibonacci(100000)]
+
+@timeme
+def pyfib():
+    [x for x in python_fb(100000)]
+
+
+
+fib()
+pyfib()
+
